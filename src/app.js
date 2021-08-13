7635eaec7f99badc51a16c38a235bd81ae7be06d
Tong Hoang Vu <tonghoangvu@outlook.com>
2021-08-13 19:52:44 +0700

    feat: improve security
    Use Helmet, HPP and rate limit

--- src/app.js
+++ src/app.js
@@ -1,16 +1,33 @@
 'use strict'
 
 const express = require('express')
+const helmet = require('helmet')
+const hpp = require('hpp')
+const rateLimit = require('express-rate-limit')
+
 const app = express()
+
+// Parser (parse before using HPP)
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 // Security
 app.disable('x-powered-by')
+app.use(helmet({
+	// Disable auto upgrade insecure request
+	contentSecurityPolicy: false
+}))
+app.use(hpp())
+
+// Rate limiting
+const limiter = rateLimit({
+	windowMs: 60 * 1000,
+	max: 60,
+	message: 'Too many requests'
+})
+app.use(limiter)
 
 // Static files
 app.use(express.static('public'))
-
-// Parser
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
 
 module.exports = app
