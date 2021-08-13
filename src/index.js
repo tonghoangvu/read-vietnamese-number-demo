315a76f75c2b530dd937cde224bd91070c8b984c
Tong Hoang Vu <tonghoangvu@outlook.com>
2021-08-13 18:23:58 +0700

    style: fix coding style

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ const app = require('./app')
 const env = require('./env')
 
 app.listen(env.PORT, () => {
-	console.log(`Server is running at ${ env.PORT }`);
+	console.log(`Server is running at ${ env.PORT }`)
 })
