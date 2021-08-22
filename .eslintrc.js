0b3ce0c4f83ba3071762fe4d3c909c3b8a2e703c
Tong Hoang Vu <tonghoangvu@outlook.com>
2021-08-22 20:30:21 +0700

    feat: setup Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,10 @@ module.exports = {
 		es2021: true,
 		commonjs: true
 	},
-	extends: 'eslint:recommended',
+	extends: [
+		'eslint:recommended',
+		'prettier'
+	],
 	parserOptions: {
 		'ecmaVersion': 12
 	},
